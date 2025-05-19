6aa82b96ee1b1e594968c333df5b444127be2022
Borbely Daniel <37481639+BorbelyDaniel@users.noreply.github.com>
2025-05-19 21:42:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,5 @@ st.title("Text Classification with Keras & Embeddings")
 user_input = st.text_area("Enter text to classify:")
 
 if st.button("Predict"):
-    preds = keras_model.predict(embed_model.predict(np.array([le.transform(user_input.split(" ")[:20])])).reshape(1, -1)) 
+    preds = keras_model.predict(embed_model.predict(np.array([le.transform(user_input.split(" ")[:20])])).reshape(1, -1))[0] 
     st.success(f"Prediction: Black:{preds[0]} Draw:{preds[1]} White:{preds[2]}")
